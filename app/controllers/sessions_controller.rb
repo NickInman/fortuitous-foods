971ae2bf88e378a3bff46ebd787fd57e789a7fb6
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-02-22 17:35:15 -0800

    alert/nav updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
 	    session[:user_id] = @user.id
 	    redirect_to user_path(@user)
 	  else
-	    redirect_to signin_path
+      
+	    redirect_to login_path, alert: "Login info invalid"
 	  end
 	end
 
