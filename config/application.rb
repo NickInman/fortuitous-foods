7b5363f4e8b7ce844755d1df94bde32a9f357416
Nick Inman <ninman@Nicks-MacBook-Pro.local>
2019-05-21 22:06:55 -0700

    heroku

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module FortuitousFoods
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.serve_static_files = true
+    config.public_file_server.enabled
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
