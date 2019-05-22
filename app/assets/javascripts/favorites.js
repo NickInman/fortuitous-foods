82fdb0f0f19e10243fc77cd6ba154b744b63e44f
Nick Inman <ninman@Nicks-MacBook-Pro.local>
2019-05-21 20:17:53 -0700

    updates

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -3,6 +3,7 @@ $(function(){
     e.preventDefault();
     $(".display_favorites").empty();
     $.get(this.href).then(function(json){
+      console.log('a')
       json.forEach(function(favorite){
         $(".display_favorites").append("<div class='rest_card'><h4><b>" + favorite.name + "</b></h4>" +
         "<img src=" + favorite.image_url + " width='350' height='350'>" +
@@ -10,8 +11,15 @@ $(function(){
         "<p>" + favorite.review_count + " reviews, "+ favorite.rating + "/5 stars</p>" +
         "<p>Price: " + favorite.price + "</p>" +
         "<p>" + favorite.city + ", " + favorite.zip + "</p>" +
-        "<p>" + (favorite.name.link(favorite.url)) + "</p></div>")
+        "<p>" + (favorite.name.link(favorite.url)) + "</p></div>" +
+        '<button class="likes"> 0 </button>')
       })
+      $(".likes").on("click", function(){
+    
+        this.textContent = parseInt(this.textContent.trim()) + 1
+      })
+      console.log('b')
     })
   })
+
 })
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,7 +11,6 @@
   end
 
   def new
-    @restaurant = Restaurant.new
   end
 
   def show
